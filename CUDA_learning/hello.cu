fcd2f33062b44f4f605554a81efbd59ebd369c48
tiagonapoli <napoli.tiago@hotmail.com>
2018-06-13 20:20:06 -0300

    asd

--- CUDA_learning/hello.cu
+++ CUDA_learning/hello.cu
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #include <assert.h>
 #include <cuda_runtime.h>
+#include <helper_functions.h>
+#include <helper_cuda.h>
 
-__global__ void testKernel(int val) {
-	printf("[%d, %d]:\t\tValue is:%d\n", blockIdx.y*gridDim.x+blockIdx.x,\
-										 threadIdx.z*blockDim.x*blockDim.y+threadIdx.y*blockDim.x+threadIdx.x,\
-										 val);
+#ifndef MAX
+#define MAX(a,b) (a > b ? a : b)
+#endif
+
+__global__ void testKernel(int val)
+{
+    printf("[%d, %d]:\t\tValue is:%d\n",\
+            blockIdx.y*gridDim.x+blockIdx.x,\
+            threadIdx.z*blockDim.x*blockDim.y+threadIdx.y*blockDim.x+threadIdx.x,\
+            val);
 }
 
-int main(int argc, char **argv) {
-	int devID;
-	cudaDeviceProp props;
+int main(int argc, char **argv)
+{
+    int devID;
+    cudaDeviceProp props;
 
-	// This will pick the best possible CUDA capable device
-	devID = findCudaDevice(argc, (const char **)argv);
+    // This will pick the best possible CUDA capable device
+    devID = findCudaDevice(argc, (const char **)argv);
 
-	//Get GPU information
-	checkCudaErrors(cudaGetDevice(&devID));
-	checkCudaErrors(cudaGetDeviceProperties(&props, devID));
-	printf("Device %d: \"%s\" with Compute %d.%d capability\n",
-	devID, props.name, props.major, props.minor);
+    //Get GPU information
+    checkCudaErrors(cudaGetDevice(&devID));
+    checkCudaErrors(cudaGetDeviceProperties(&props, devID));
+    printf("Device %d: \"%s\" with Compute %d.%d capability\n",
+           devID, props.name, props.major, props.minor);
 
-	printf("printf() is called. Output:\n\n");
+    printf("printf() is called. Output:\n\n");
 
-	//Kernel configuration, where a two-dimensional grid and
-	//three-dimensional blocks are configured.
-	dim3 dimGrid(2, 2);
-	dim3 dimBlock(2, 2, 2);
-	testKernel<<<dimGrid, dimBlock>>>(10);
-	cudaDeviceSynchronize();
+    //Kernel configuration, where a two-dimensional grid and
+    //three-dimensional blocks are configured.
+    dim3 dimGrid(2, 2);
+    dim3 dimBlock(2, 2, 2);
+    testKernel<<<dimGrid, dimBlock>>>(10);
+    cudaDeviceSynchronize();
 
-	return 0;
-
+    return EXIT_SUCCESS;
 }
+
